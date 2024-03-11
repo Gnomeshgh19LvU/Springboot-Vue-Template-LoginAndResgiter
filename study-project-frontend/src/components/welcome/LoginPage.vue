a48ed8f63704f28dc15482502cbc941d64ab3a4a
Gnomeshgh19LvU <1662859514@qq.com>
2024-03-12 02:05:55 +0800

    后端功能完成，前端页面完成

---
+++ study-project-frontend/src/components/welcome/LoginPage.vue
@@ -0,0 +1,54 @@
+<script setup>
+
+import {Lock, User} from "@element-plus/icons-vue";
+</script>
+
+<template>
+  <div style="text-align: center;margin: 0 20px">
+
+    <div style="margin-top: 150px">
+      <div style="font-size: 25px; font-weight: bold">登录</div>
+      <div style="font-size: 14px;color: grey">在进入系统之前请先输入用户名和密码进行登录</div>
+    </div>
+
+    <div style="margin-top: 50px">
+      <el-input type="text" placeholder="用户名/邮箱">
+        <template #prefix>
+          <el-icon><User /></el-icon>
+        </template>
+      </el-input>
+
+      <el-input type="password" style="margin-top: 10px" placeholder="密码">
+        <template #prefix>
+          <el-icon><Lock /></el-icon>
+        </template>
+      </el-input>
+    </div>
+
+    <el-row style="margin-top: 5px">
+      <el-col :span="12" style="text-align: left">
+        <el-checkbox v-model="checked1" label="记住我"/>
+      </el-col>
+      <el-col :span="12" style="text-align: right">
+        <el-link>忘记密码</el-link>
+      </el-col>
+    </el-row>
+
+    <div style="margin-top: 40px">
+      <el-button type="primary" style="width: 100%;margin-top: 10px">立即登录</el-button>
+    </div>
+
+    <el-divider>
+      <span style="color: grey">没有账号</span>
+    </el-divider>
+
+    <div>
+      <el-button type="text" style="width: 100%;margin-top: 10px">立即注册</el-button>
+    </div>
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ study-project-frontend/src/views/WelcomeView.vue
@@ -0,0 +1,39 @@
+<script setup>
+
+
+</script>
+
+<template>
+
+  <div style="width: 100vw;height: 100vh;overflow: hidden;display: flex">
+
+    <!--    页面左边 -> 图片页面-->
+    <div style="flex: 1">
+      <el-image style="width: 100%;height: 100%" fit="cover"
+                src="https://ts1.cn.mm.bing.net/th/id/R-C.f23301faaa645ddb2c90381bb368b778?rik=HGiferJo1Msdeg&riu=http%3a%2f%2fimg.pconline.com.cn%2fimages%2fupload%2fupc%2ftx%2fwallpaper%2f1208%2f09%2fc1%2f12788328_1344484585308.jpg&ehk=HM8tHpdH2UPCwT65cB5GTQ0m3QJKjN%2bRVQz3NfP%2bU2c%3d&risl=&pid=ImgRaw&r=0"/>
+    </div >
+
+    <div class="welcome-title">
+      <div style="font-size: 30px;font-weight: bold">欢迎来到我们的学习平台</div>
+      <div style="margin-top: 10px">在这里你可以学习如何使用java，如何搭建网站。并且与java之父密切交流</div>
+      <div style="margin-top: 10px">在这里你可以同性交友，因为都是男的，没有学java的妹崽</div>
+
+    </div>
+
+    <!--    页面右边 ->登录页面-->
+    <div style="width: 400px;background-color: white;">
+      <router-view/>
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+ .welcome-title {
+  position: absolute;
+  bottom: 30px;
+  left: 30px;
+  color: white;
+  text-shadow: 0 0 10px black;
+}
+</style>
